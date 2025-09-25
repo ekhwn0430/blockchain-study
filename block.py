79042eb576d7e20d21e3a4c85c869800518729d5
ekhwn0430 <100757880+ekhwn0430@users.noreply.github.com>
2025-09-25 11:40:08 +0900

    Update block.py

--- block.py
+++ block.py
@@ -1,5 +1,33 @@
 from hashlib import sha256
-import datetime
+import random
+import string
 
 
-block_body = {}
+a = sha256(str("hello").encode()).hexdigest()
+b = sha256(str("1234").encode()).hexdigest()
+c = sha256(str("home").encode()).hexdigest()
+
+print(a)
+print(b)
+print(c)
+print()
+
+
+problem_word = "a"
+problem_difficulty = 2
+
+start_nonce = random.choice(string.ascii_letters)
+
+i = 0
+while True:
+    nonce = start_nonce + str(i)
+    nonce_result = sha256((nonce).encode()).hexdigest()
+    print(i, nonce, nonce_result)
+    
+    if nonce_result[0:problem_difficulty] == problem_word * problem_difficulty:
+        nonce = nonce_result
+        break
+    
+    i += 1
+
+print("\n" + nonce)
