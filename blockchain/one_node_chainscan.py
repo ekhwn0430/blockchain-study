04701a09f71a60408534f5cb081eea7249e0d9e1
ekhwn0430 <100757880+ekhwn0430@users.noreply.github.com>
2025-10-01 09:59:15 +0900

    Update one_node_chainscan.py

--- blockchain/one_node_chainscan.py
+++ blockchain/one_node_chainscan.py
@@ -4,3 +4,17 @@ import pandas as pd
 import requests
 import json
 import os
+
+
+app = Flask(__name__, template_folder = od.getcwd())
+
+@app.route('/')
+def index():
+    headers = {'Content-Type' : 'application/json; charset=utf-8'}
+    res = requests.get("http://localhost:5000/chain", headers = headers)
+
+    status_json = json.loads(res.text)
+    df_scan = pd.DataFrame(status_json['chain'])
+    return render_template('/one_node_scan.html', df_scan = df_scan, block_len = len(df_scan))
+
+app.run(port=8080)
