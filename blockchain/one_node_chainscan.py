a1975e75e0e5e925101d6f2c91c4b4cc9d30518f
ekhwn0430 <100757880+ekhwn0430@users.noreply.github.com>
2025-10-01 14:49:54 +0900

    Update one_node_chainscan.py

--- blockchain/one_node_chainscan.py
+++ blockchain/one_node_chainscan.py
@@ -8,11 +8,15 @@ import os
 
 app = Flask(__name__, template_folder = od.getcwd())
 
+# 블록 체인 내 블록 정보를 제공하는 url에 request 방식으로 데이터를 요청
+# 요청 결과 데이터(res.text)를 json으로 로드
+# 결과 데이터를 데이터프레임으로 정리
+# front 구성 내용이 담길 one_node_scan.html 파일에 데이터프레임 정보와 블록의 길이 제공
 @app.route('/')
 def index():
     headers = {'Content-Type' : 'application/json; charset=utf-8'}
     res = requests.get("http://localhost:5000/chain", headers = headers)
-
+    
     status_json = json.loads(res.text)
     df_scan = pd.DataFrame(status_json['chain'])
     return render_template('/one_node_scan.html', df_scan = df_scan, block_len = len(df_scan))
